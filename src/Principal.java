74913d8299b4e086dec4222cc7ccf6f60b4b03f4
lubuntu <africavillaverdel26@gmail.com>
2019-04-05 16:16:17 +0000

    he hecho la escena principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,34 @@
-public class Principal {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+public class Principal extends Application{
+	
+	
+	// Datos del Programa y Tamaño de Pantalla
+		static final double VERSION = 0.1;
+
+		static final double ANCHO = 1024;// px
+		static final double ALTO = 714;// px
+
+
+		public static void main(String[] args) {
+			launch(args);
+		}
+
+		
+		@Override
+		public void start(Stage ventana) {
+			// 1.1 Crear la ventana
+			ventana.setWidth(ANCHO);
+			ventana.setHeight(ALTO);
+			ventana.setTitle("KAY " + VERSION);
+			ventana.setResizable(false);
+
+			// 1.2 Crear la escena
+	//ventana.setScene(new Escena1(null));
+			ventana.show();
+		}
+
 	}
 
-}
+	
