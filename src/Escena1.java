c22666b3595643bc0ba9eeb80104e9831574d714
hongol848 <hongol848@gmail.com>
2019-04-12 18:41:51 +0200

    fe

--- src/Escena1.java
+++ src/Escena1.java
@@ -8,11 +8,18 @@ public class Escena1 extends Scene {
 	// Atributos de la Escena
 	static final Group RAIZ = new Group();
 	static final Image IMAGEN = new Image(Escena1.class.getClassLoader().getResourceAsStream("recursos/imagen1.png"));
-
+	
 	public Escena1() {
 		super (RAIZ);
 		ImageView iv = new ImageView(IMAGEN);
 		RAIZ.getChildren().add(iv);
+		Jugador player = new Jugador();
+		player.setX(100);
+		player.setY(100);
+		player.setFitWidth(60);
+		player.setFitHeight(60);
+		RAIZ.getChildren().add(player);
+		
 	}
 
 }
